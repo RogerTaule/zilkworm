ec6c030fa7741631bc35041b763ffc70fd3535b1
Paweł Bylica <pawel@ethereum.org>
2025-08-29 14:00:42 +0200

    invoke global constructors from the main wrapper function

--- guest_program/src/wrapper.cpp
+++ guest_program/src/wrapper.cpp
@@ -6,7 +6,24 @@
 #include <array>
 #include "include/sp1_syscalls.hpp"
 
+/* These magic symbols are provided by the linker.  */
+extern void (*__preinit_array_start []) (void);
+extern void (*__preinit_array_end []) (void);
+extern void (*__init_array_start []) (void);
+extern void (*__init_array_end []) (void);
+extern void (*__fini_array_start []) (void);
+extern void (*__fini_array_end []) (void);
+
 extern "C" uint64_t sample_run_wrapped(uint32_t n, rust::Str jsonStr1) {
+
+    // Call global constructors because SP1's _start function doesn't.
+    for (auto p = __preinit_array_start; p != __preinit_array_end; ++p) {
+        (*p)();
+    }
+    for (auto p = __init_array_start; p != __init_array_end; ++p) {
+        (*p)();
+    }
+
     //Initialize a state_transition object with one Shanghai Transaction - within silkworm
     auto state_transition = silkworm::cmd::state_transition::StateTransition(jsonStr1.data(), false, true);
 
