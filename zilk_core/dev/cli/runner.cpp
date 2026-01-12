47695d66510841844cb1e3c02c4c952026670af0
Paweł Bylica <pawel.bylica@erigon.tech>
2026-01-12 13:13:44 +0100

    Improve state_transition CLI

--- zilk_core/dev/cli/runner.cpp
+++ zilk_core/dev/cli/runner.cpp
@@ -3,7 +3,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include <filesystem>
-
 #include <fstream>
 
 #include "../state_transition.hpp"
@@ -13,27 +12,37 @@ using namespace silkworm::cmd::state_transition;
 int main(int argc, const char* argv[]) {
     try {
         if (argc < 2) {
-            std::cerr << "Usage: " << argv[0] << " <path_to_unified_rlp_bin>\n"
-                      << "[is_test]";
+            std::cerr << "Usage: " << argv[0] << " <path_to_unified_rlp_bin>|<test.json>|<test_dir>\n";
             return 1;
         }
         const std::string file_path = argv[1];
-        std::ifstream file(file_path);
-        if (!file.is_open()) {
-            throw std::runtime_error("Could not open file: " + file_path);
-        }
-        auto input_str = std::string((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
 
-        if (argc == 3 && std::string(argv[2]) == "is_test") {
-            auto state_transition = StateTransition(input_str, false, true);
+        if (file_path.ends_with(".json") || std::filesystem::is_directory(file_path)) {
+            std::cerr << "JSON\n";
+            std::ifstream file(file_path);
+            const auto input_str = std::string(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>());
+            if (file.fail()) {
+                throw std::runtime_error("Failed to read file: " + file_path);
+            }
+            const auto terminate_on_error = false;
+            const auto show_diagnostics = true;
+            auto state_transition = StateTransition(input_str, terminate_on_error, show_diagnostics);
             auto total_gas = state_transition.run(1, true);
             std::cout << "Cumulative Gas Used: " << total_gas << "\n";
-        } else {
-            auto state_transition = silkworm::cmd::state_transition::StateTransition(std::move(input_str));
-            auto total_gas = state_transition.run_rlp();
-            printf("Cumulative Gas Used: %lu\n", total_gas);
+            return 0;
         }
 
+        {
+            std::ifstream file(file_path, std::ios::binary);
+            const auto input_str = std::string(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>());
+            if (file.fail()) {
+                throw std::runtime_error("Failed to read file: " + file_path);
+            }
+            auto state_transition = StateTransition(input_str);
+            auto total_gas = state_transition.run_rlp();
+            std::cout << "Cumulative Gas Used: " << total_gas << "\n";
+            return 0;
+        }
         return 0;
     } catch (const std::exception& e) {
         // code to handle exceptions of type std::exception and its derived classes
